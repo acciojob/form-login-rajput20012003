5d760617df83e91fd32942e7c9ffd18b82389322
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-18 12:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+
+	function getFormvalue() {
+    // Accessing the input fields by their IDs
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+
+    // Concatenating first and last names
+    const fullName = firstName + " " + lastName;
+
+    // Displaying the full name using alert
+    alert(fullName);
+}
+
 
 }
